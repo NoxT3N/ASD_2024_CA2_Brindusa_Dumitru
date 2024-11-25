bbdba8871c63eea913ee9b0ef41b632acf3ea729
Felix <145358483+NoxT3N@users.noreply.github.com>
2024-11-25 23:21:15 +0000

    Add project files.

---
+++ ASD_CA2_Brindusa_Dumitru/ASD_CA2_Brindusa_Dumitru.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ASDCA2BrindusaDumitru
+{
+	TEST_CLASS(ASDCA2BrindusaDumitru)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
